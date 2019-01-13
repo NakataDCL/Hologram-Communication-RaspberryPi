0e3da564ff619ddb3f0b148e0f7aacbb358cecf1
Naoki Nakata <>
2019-01-14 00:31:23 +0900

    [update] 表示するウィンドウのサイズを指定できるように修正

--- dev/DisplayWebcamImage.py
+++ dev/DisplayWebcamImage.py
@@ -1,11 +1,16 @@
 import cv2
 
+img_width = 800
+img_height = 600
+window_name = str(img_width) + ' x ' + str(img_height)
+
 # VideoCapture オブジェクトを取得
 capture = cv2.VideoCapture(0)
 
 while True:
     ret, frame = capture.read()
-    cv2.imshow('frame', frame)
+    scaled_image = cv2.resize(frame, dsize=(img_width, img_height))
+    cv2.imshow(window_name, scaled_image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
