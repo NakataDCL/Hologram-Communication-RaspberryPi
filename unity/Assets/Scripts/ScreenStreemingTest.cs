f5fbe570715594e118830c1fdb66a35103779a4f
Naoki Nakata <>
2019-01-23 18:48:10 +0900

    [update] Unity で画面のスクリーンショットを取得する機能を実装

---
+++ unity/Assets/Scripts/ScreenStreemingTest.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Cache;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScreenStreemingTest : MonoBehaviour {
+
+	private TcpListener _listener;
+	private readonly List<TcpClient> _clients = new List<TcpClient> ();
+
+	private Texture2D _texture;
+
+	[SerializeField]
+	public RawImage raw;
+
+	// Use this for initialization
+	void Start () {
+		// Serverの待ち受けを開始
+		StartServerListening ("127.0.0.1", 8081);
+
+		// rawimageの設定
+		raw.color = Color.white;
+		raw.texture = _texture;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// スクリーンショットの更新を開始
+		StartCoroutine ("LoadScreenshot");
+	}
+
+	// ソケット接続準備、待機
+	protected void StartServerListening (string host, int port) {
+		Debug.Log ("ipAddress: " + host + ", port: " + port);
+		var ip = IPAddress.Parse (host);
+		_listener = new TcpListener (ip, port);
+
+		_listener.Start ();
+		_listener.BeginAcceptSocket (DoAcceptTcpClientCallback, _listener);
+	}
+
+	// クライアントからの接続処理
+	private void DoAcceptTcpClientCallback (IAsyncResult ar) {
+		TcpListener listener = (TcpListener) ar.AsyncState;
+		TcpClient client = listener.EndAcceptTcpClient (ar);
+		_clients.Add (client);
+		Debug.Log ("Connect: " + client.Client.RemoteEndPoint);
+
+		// 接続が確立したら次の人を受け付ける
+		listener.BeginAcceptSocket (DoAcceptTcpClientCallback, listener);
+
+		// bytestreamの読み込み
+		NetworkStream stream = client.GetStream ();
+
+		// クライアントとの接続が切れるまで繰り返す
+		while (client.Connected) {
+			// 画面のスクリーンショットを取得
+
+			// base64に変換
+
+			// バイナリのサイズを通知(int: 4[byte])
+
+			// バイナリデータを送信
+
+			// クライアントの接続が切れたら
+			if (client.Client.Poll (1000, SelectMode.SelectRead) && (client.Client.Available == 0)) {
+				Debug.Log ("Disconnect: " + client.Client.RemoteEndPoint);
+				client.Close ();
+				_clients.Remove (client);
+				break;
+			}
+		}
+	}
+
+	// 終了時処理
+	protected void OnApplicationQuit () {
+		foreach (var client in _clients) {
+			client.Close ();
+		}
+
+		_listener.Stop ();
+	}
+
+	IEnumerator LoadScreenshot () {
+		yield return new WaitForEndOfFrame ();
+
+		_texture = new Texture2D (Screen.width / 2, Screen.height / 2);
+		Debug.Log (Screen.width / 2 + ", " + Screen.height / 2);
+
+		_texture.ReadPixels (new Rect (0, 0, Screen.width / 2, Screen.height / 2), 0, 0);
+		_texture.Apply ();
+
+		raw.texture = _texture;
+	}
+}
--- unity/Assets/Scripts/TCPServer.cs
+++ unity/Assets/Scripts/TCPServer.cs
@@ -16,6 +16,8 @@ public class TCPServer : MonoBehaviour {
   private TcpListener _listener;
   private readonly List<TcpClient> _clients = new List<TcpClient> ();
 
+  private int _count = 0;
+
   // Use this for initialization
   void Start () {
     // logファイルのパスを取得
@@ -23,7 +25,7 @@ public class TCPServer : MonoBehaviour {
     _byteLogFilePath = Application.dataPath + "/Log/log";
 
     // 画像ファイルの保存先のパスを取得
-    _pngPath = Application.dataPath + "/Img/img.png";
+    _pngPath = Application.dataPath + "/Img/img";
 
     // Serverの待ち受けを開始
     StartServerListening ("127.0.0.1", 8080);
@@ -51,33 +53,47 @@ public class TCPServer : MonoBehaviour {
     // bytestreamの読み込み
     NetworkStream stream = client.GetStream ();
 
-    // バイナリデータのサイズをチェック(int: 4[byte])
-    byte[] b_data_size = new byte[4];
-    stream.Read (b_data_size, 0, 4);
-    int data_size = BitConverter.ToInt32 (b_data_size, 0);
-    Debug.Log ("data size: " + data_size);
+    // クライアントとの接続が切れるまで繰り返す
+    while (client.Connected) {
+      // バイナリデータのサイズをチェック(int: 4[byte])
+      byte[] b_data_size = new byte[4];
+      stream.Read (b_data_size, 0, 4);
+      int data_size = BitConverter.ToInt32 (b_data_size, 0);
+      Debug.Log ("data size: " + data_size);
 
-    // バイナリデータを受信
-    byte[] b_data = new byte[data_size];
-    stream.Read (b_data, 0, data_size);
+      // バイナリデータを受信
+      byte[] b_data = new byte[data_size];
+      stream.Read (b_data, 0, data_size);
+      Debug.Log ("receive data: " + b_data.Length + "[byte]");
 
-    // logファイルに受信したデータを出力する
-    ByteSave (b_data);
+      // logファイルに受信したデータを出力する
+      // ByteSave (b_data);
 
-    // 受信したデータをbase64デコード(画像データ)
-    string base64Text = Encoding.UTF8.GetString (b_data);
-    byte[] decodedByte = Convert.FromBase64String (base64Text);
+      // 受信したデータをbase64デコード(画像データ)
+      string base64Text = Encoding.UTF8.GetString (b_data);
+      byte[] decodedByte = Convert.FromBase64String (base64Text);
+      Debug.Log ("decode data: " + decodedByte.Length + "[byte]");
 
-    // 画像データを保存
-    PngSave (decodedByte);
+      // 画像データを保存
+      PngSave (decodedByte);
 
-    // 受信したことをクライアントに通知
-    var resMsg = "successfully received";
-    var body = Encoding.UTF8.GetBytes (resMsg);
-    try {
-      stream.Write (body, 0, body.Length);
-    } finally {
-      _clients.Remove (client);
+      // 受信したことをクライアントに通知
+      string resMsg = "successfully received";
+      var body = Encoding.UTF8.GetBytes (resMsg);
+      try {
+        Debug.Log ("send message: " + resMsg);
+        stream.Write (body, 0, body.Length);
+      } finally {
+        //_clients.Remove (client);
+      }
+
+      // クライアントの接続が切れたら
+      if (client.Client.Poll (1000, SelectMode.SelectRead) && (client.Client.Available == 0)) {
+        Debug.Log ("Disconnect: " + client.Client.RemoteEndPoint);
+        client.Close ();
+        _clients.Remove (client);
+        break;
+      }
     }
   }
 
@@ -117,7 +133,8 @@ public class TCPServer : MonoBehaviour {
   public void PngSave (byte[] b) {
     Debug.Log ("save image");
 
-    FileInfo fi = new FileInfo (_pngPath);
+    FileInfo fi = new FileInfo (_pngPath + _count + ".png");
+    _count++;
 
     FileStream fs = fi.Create ();
     BinaryWriter writer = new BinaryWriter (fs, Encoding.UTF8);
